381cff8aea6c1aa97f553abe4bdaab071371c3b2
Gregor Becker <gregor@codedredd.de>
2022-04-20 16:47:18 +0200

    docs: new documentation

--- docs/v3/docus.config.ts
+++ docs/v3/docus.config.ts
@@ -1,1 +1,19 @@
-export default defineDocus
+export default {
+    title: '@nuxtjs/supabase',
+    url: 'https://supabase.nuxtjs.org',
+    theme: {
+        colors: {
+            primary: '#34d399',
+            prism: {
+                background: '#F4F4F5 dark:#1F2937'
+            }
+        },
+        header: {
+            title: false,
+            logo: {
+                light: '/logo-light.svg',
+                dark: '/logo-dark.svg'
+            }
+        }
+    }
+}
